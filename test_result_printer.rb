de10f3fe3ea1aa54900c5c55664e2f4c7904e051
Ekaterina Ionidi <cat81.06@gmail.com>
2016-11-05 13:24:00 +0300

    Update test_result_printer.rb

--- test_result_printer.rb
+++ test_result_printer.rb
@@ -1,30 +1,26 @@
 class ResultPrinter
+
   def initialize
     current_path = File.dirname(__FILE__)
     file_name = current_path + "/results.txt"
-    if !File.exist?(file_name)
-      abort "Невозможно загрузить результаты!"
-    end
+
+    abort "Невозможно загрузить результаты!" if !File.exist?(file_name)
+
     f = File.new(file_name)
     @results = f.readlines
     f.close
   end
+
   def print_result(test)
+
     puts "\nВаш результат теста - #{test.points} баллов:"
-    if (test.points >= 30)
-      puts @results[0]
-    elsif (test.points >= 25 && test.points <= 29)
-      puts @results[1]
-    elsif (test.points >= 19 && test.points <= 24)
-      puts @results[2]
-    elsif (test.points >= 14 && test.points <= 18)
-      puts @results[3]
-    elsif (test.points >= 9 && test.points <= 13)
-      puts @results[4]
-    elsif (test.points >= 4 && test.points <= 8)
-      puts @results[5]
-    else
-      puts @results[6]
-    end
+
+    puts @results[0] if (test.points >= 30)
+    puts @results[1] if (test.points >= 25 && test.points <= 29)
+    puts @results[2] if (test.points >= 19 && test.points <= 24)
+    puts @results[3] if (test.points >= 14 && test.points <= 18)
+    puts @results[4] if (test.points >= 9 && test.points <= 13)
+    puts @results[5] if (test.points >= 4 && test.points <= 8)
+    puts @results[6] if (test.points < 4)
   end
 end
